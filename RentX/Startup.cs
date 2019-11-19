180e18d1b79bee19deaec50103ee71b2d29a2f14
danny serra <danielserradev@gmail.com>
2019-11-19 10:39:25 -0600

    init commit

---
+++ RentX/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RentX.Startup))]
+namespace RentX
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
